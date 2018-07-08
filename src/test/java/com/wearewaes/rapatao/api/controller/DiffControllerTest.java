5b59ec2d0689460466a1d498b0d506628277b278
Luiz H. Rapatao <luiz@rapatao.com>
2018-07-08 14:35:10 -0300

    adds missing integration tests

--- src/test/java/com/wearewaes/rapatao/api/controller/DiffControllerTest.java
+++ src/test/java/com/wearewaes/rapatao/api/controller/DiffControllerTest.java
@@ -2,6 +2,7 @@ package com.wearewaes.rapatao.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wearewaes.rapatao.api.domain.RequestData;
+import com.wearewaes.rapatao.api.domain.ResponseType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,27 +60,105 @@ public class DiffControllerTest {
                 .get("/diff/{id}", diffId)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("EQUALS")));
+                .andExpect(content().string(containsString(ResponseType.EQUALS.toString())));
     }
 
     @Test
-    public void shouldAddBothSidesWithDifferentSizesAndGetTheResultAsDifferentSize() {
-        throw new RuntimeException();
+    public void shouldAddBothSidesWithDifferentSizesAndGetTheResultAsDifferentSize() throws Exception {
+        final String diffId = UUID.randomUUID().toString();
+
+        final String leftSideContent = objectMapper.writeValueAsString(RequestData.builder()
+                .value("d2UgYXJlIHdhZXM=")
+                .build());
+
+        final String rightSideContent = objectMapper.writeValueAsString(RequestData.builder()
+                .value("eWVzLCB3ZSBhcmUgd2Flcw==")
+                .build());
+
+        mvc.perform(MockMvcRequestBuilders
+                .put("/diff/{id}/left", diffId)
+                .content(leftSideContent)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders
+                .put("/diff/{id}/right", diffId)
+                .content(rightSideContent)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/diff/{id}", diffId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(ResponseType.DIFFERENT_SIZE.toString())));
     }
 
     @Test
-    public void shouldAddBothSidesWithSameSizeAndGetTheResultAsDifferentContent() {
-        throw new RuntimeException();
+    public void shouldAddBothSidesWithSameSizeAndGetTheResultAsDifferentContent() throws Exception {
+        final String diffId = UUID.randomUUID().toString();
+
+        final String leftSideContent = objectMapper.writeValueAsString(RequestData.builder()
+                .value("eWVzLCB3ZSBhcmUgd2Flcw==")
+                .build());
+
+        final String rightSideContent = objectMapper.writeValueAsString(RequestData.builder()
+                .value("bm93LCB3ZSBhcmUgd2Flcw==")
+                .build());
+
+        mvc.perform(MockMvcRequestBuilders
+                .put("/diff/{id}/left", diffId)
+                .content(leftSideContent)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders
+                .put("/diff/{id}/right", diffId)
+                .content(rightSideContent)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/diff/{id}", diffId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(ResponseType.DIFFERENT_CONTENT.toString())))
+                .andExpect(content().string(containsString("Offsets: 0,1,2,3 - Length: 24")));
     }
 
     @Test
-    public void shouldAddOneSideAndGotErrorWhenGetDiff() {
-        throw new RuntimeException();
+    public void shouldAddOneSideAndGotErrorWhenGetDiff() throws Exception {
+        final String diffId = UUID.randomUUID().toString();
+
+        final String leftSideContent = objectMapper.writeValueAsString(RequestData.builder()
+                .value("d2UgYXJlIHdhZXM=")
+                .build());
+
+        mvc.perform(MockMvcRequestBuilders
+                .put("/diff/{id}/left", diffId)
+                .content(leftSideContent)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/diff/{id}", diffId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
     }
 
     @Test
-    public void shouldGetNotFoundWhenGetDiffFromInvalidIdentifier() {
-        throw new RuntimeException();
+    public void shouldGetNotFoundWhenGetDiffFromInvalidIdentifier() throws Exception {
+        final String diffId = UUID.randomUUID().toString();
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/diff/{id}", diffId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 
     @Test
